d6727ceb4f19da0037a3b57007ba966b00789889
Jacob Angulo <jacobangulo@hotmail.com>
2019-04-03 12:39:35 -0700

    server.js file created and exported to index.js where it listens on a localhost server with port defined in a dotenv file

--- data/migrations/20190403121757_users_table.js
+++ data/migrations/20190403121757_users_table.js
@@ -1,8 +1,16 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("users", tbl => {
+    tbl.increments();
+
+    tbl
+      .string("username")
+      .notNullable()
+      .unique();
+
+    tbl.string("password").notNullable()
+
+    tbl.string('department')
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = function(knex, Promise) {};
